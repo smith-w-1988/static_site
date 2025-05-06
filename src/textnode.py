c1517f5863b725c3afde0d0061fdd6f4ee99d872
smith-w-1988 <smith-w@live.co.uk>
2025-05-06 12:09:18 +0100

    initial TextNode class setup

---
+++ src/textnode.py
@@ -0,0 +1,24 @@
+from enum import Enum
+
+class Bender(Enum):
+    AIR_BENDER = "air"
+    WATER_BENDER = "water"
+    EARTH_BENDER = "earth"
+    FIRE_BENDER = "fire"
+
+class TextNode():
+    def __init__(self, text, text_type, url=None):
+        self.text = text
+        self.text_type = text_type
+        self.url = url
+
+    def __eq__(self, othernode):
+        if self.text == othernode.text \
+            and self.text_type == othernode.text_type \
+            and self.url == othernode.url:
+            return True
+        else:
+            return False
+        
+    def __repr__(self):
+        return f"TextNode({self.text}, {self.text_type}, {self.url})"
